5fa4d08ebe72f5caa509b4800631989d22c67bb8
David Maher <sunookitsune@kitsunet.org>
2019-08-11 17:25:48 -0400

    Initial commit, basically on par with C++ version, missing commandline switches

---
+++ Digit.go
@@ -0,0 +1,357 @@
+package main
+
+import (
+	"image"
+	"image/color"
+	"image/draw"
+)
+
+const (
+	sA int = 0
+	sB int = 1
+	sC int = 2
+	sD int = 3
+	sE int = 4
+	sF int = 5
+	sG int = 6
+)
+
+var (
+	digitBits = [10][7]bool {{true, true, true, true, true, true, false},
+		{false, true, true, false, false, false, false},
+		{true, true, false, true, true, false, true},
+		{true, true, true, true, false, false, true},
+		{false, true, true, false, false, true, true},
+		{true, false, true, true, false, true, true},
+		{true, false, true, true, true, true, true},
+		{true, true, true, false, false, false, false},
+		{true, true, true, true, true, true, true},
+		{true, true, true, true, false, true, true}}
+)
+
+type Digit struct {
+	segLength int
+	value int
+	img draw.Image
+	frame int
+}
+
+func NewDigit(segLength int) Digit {
+	width := segLength + 2
+	height := segLength * 2 + 3
+	img := image.NewRGBA(image.Rect(0, 0, width, height))
+	d := Digit{segLength, -1, img, 0}
+	return d
+}
+
+func NewColon(segLength int) Digit {
+	width := 2
+	height := segLength * 2 + 3
+	img := image.NewRGBA(image.Rect(0, 0, width, height))
+	d := Digit{segLength, -1, img, 0}
+	//For some reason I can't find, the height needs to be 1 instead of 2 to be 2 pixels tall
+	d.drawRect(0, segLength + 3, 2, 1, true)
+	d.drawRect(0, segLength + 1 - 3, 2, 1, true)
+	return d
+}
+
+func (d *Digit) drawPixel(x int, y int, active bool) {
+	var col color.RGBA
+	if active {
+		col = color.RGBA{0, 0, 255, 255}
+	} else {
+		col = color.RGBA{0, 0, 0, 255}
+	}
+	d.img.Set(x, d.img.Bounds().Max.Y - y - 1, col)
+}
+
+func (d *Digit) drawRect(x int, y int, w int, h int, active bool) {
+	for curX := x; curX <= x + w; curX++ {
+		for curY := y; curY <= y + h; curY++ {
+			d.drawPixel(curX, curY, active)
+		}
+	}
+}
+
+func (d *Digit) drawLine(x1 int, y1 int, x2 int, y2 int, active bool) {
+	var x, y, w, h int
+	if x1 < x2 {
+		x = x1
+		w = x2 - x1
+	} else {
+		x = x2
+		w = x1 - x2
+	}
+	if y1 < y2 {
+		y = y1
+		h = y2 - y1
+	} else {
+		y = y2
+		h = y1 - y2
+	}
+	d.drawRect(x, y, w, h, active)
+}
+
+func (d *Digit) drawSeg(seg int) {
+	switch seg {
+		case sA:
+			d.drawLine(1, d.segLength * 2 + 2, d.segLength, d.segLength * 2 + 2, true)
+		case sB:
+			d.drawLine(d.segLength + 1, d.segLength * 2 + 1, d.segLength + 1, d.segLength + 2, true)
+		case sC:
+			d.drawLine(d.segLength + 1, 1, d.segLength + 1, d.segLength, true)
+		case sD:
+			d.drawLine(1, 0, d.segLength, 0, true)
+		case sE:
+			d.drawLine(0, 1, 0, d.segLength, true)
+		case sF:
+			d.drawLine(0, d.segLength * 2 + 1, 0, d.segLength + 2, true)
+		case sG:
+			d.drawLine(1, d.segLength + 1, d.segLength, d.segLength + 1, true)
+	}
+}
+
+func (d *Digit) Draw(value int) {
+	for i := 0; i < 7; i++ {
+		if digitBits[value][i] {d.drawSeg(i)}
+	}
+	d.value = value
+	d.frame = 0
+}
+
+func (d *Digit) Morph1() bool {
+	if d.frame <= d.segLength + 1 {
+		d.drawLine(d.frame - 1, 1, d.frame - 1, d.segLength, false)
+		d.drawLine(d.frame, 1, d.frame, d.segLength, true)
+		
+		d.drawLine(d.frame - 1, d.segLength * 2 + 1, d.frame - 1, d.segLength + 2, false)
+		d.drawLine(d.frame, d.segLength * 2 + 1, d.frame, d.segLength + 2, true)
+		
+		d.drawPixel(1 + d.frame, d.segLength * 2 + 2, false)
+		d.drawPixel(1 + d.frame, 0, false)
+		d.drawPixel(1 + d.frame, d.segLength + 1, false)
+		
+		d.frame++
+		return false
+	} else {
+		d.frame = 0
+		return true
+	}
+}
+
+func (d *Digit) Morph2() bool {
+	if d.frame <= d.segLength {
+		if (d.frame < d.segLength) {
+			d.drawPixel(d.segLength - d.frame, d.segLength * 2 + 2, true)
+			d.drawPixel(d.segLength - d.frame, d.segLength + 1, true)
+			d.drawPixel(d.segLength - d.frame, 0, true)
+		}
+		
+		d.drawLine(d.segLength + 1 - d.frame, 1, d.segLength + 1 - d.frame, d.segLength, false)
+		d.drawLine(d.segLength - d.frame, 1, d.segLength - d.frame, d.segLength, true)
+		
+		d.frame++
+		return false
+	} else {
+		d.frame = 0
+		return true
+	}
+}
+
+func (d *Digit) Morph3() bool {
+	if d.frame <= d.segLength {
+		d.drawLine(d.frame, 1, d.frame, d.segLength, false)
+		d.drawLine(1 + d.frame, 1, 1 + d.frame, d.segLength, true)
+		
+		d.frame++
+		return false
+	} else {
+		d.frame = 0
+		return true
+	}
+}
+
+func (d *Digit) Morph4() bool {
+	if d.frame < d.segLength {
+		d.drawPixel(d.segLength - d.frame, d.segLength * 2 + 2, false)
+		d.drawPixel(0, d.segLength * 2 + 1 - d.frame, true)
+		d.drawPixel(1 + d.frame, 0, false)
+		
+		d.frame++
+		return false
+	} else {
+		d.frame = 0
+		return true
+	}
+}
+
+func (d *Digit) Morph5() bool {
+	if d.frame < d.segLength {
+		d.drawPixel(d.segLength + 1, d.segLength + 2 + d.frame, false)
+		d.drawPixel(d.segLength - d.frame, d.segLength * 2 + 2, true)
+		d.drawPixel(d.segLength - d.frame, 0, true)
+		
+		d.frame++
+		return false
+	} else {
+		d.frame = 0
+		return true
+	}
+}
+
+func (d *Digit) Morph6() bool {
+	if d.frame <= d.segLength {
+		d.drawLine(d.segLength - d.frame, 1, d.segLength - d.frame, d.segLength, true)
+		if d.frame > 0 {
+			d.drawLine(d.segLength - d.frame + 1, 1, d.segLength - d.frame + 1, d.segLength, false)
+		}
+		
+		d.frame++
+		return false
+	} else {
+		d.frame = 0
+		return true
+	}
+}
+
+func (d *Digit) Morph7() bool {
+	if d.frame <= d.segLength + 1 {
+		d.drawLine(d.frame - 1, 1, d.frame - 1, d.segLength, false)
+		d.drawLine(d.frame, 1, d.frame, d.segLength, true)
+		
+		d.drawLine(d.frame - 1, d.segLength * 2 + 1, d.frame - 1, d.segLength + 2, false)
+		d.drawLine(d.frame, d.segLength * 2 + 1, d.frame, d.segLength + 2, true)
+		
+		d.drawPixel(1 + d.frame, 0, false)
+		d.drawPixel(1 + d.frame, d.segLength + 1, false)
+		
+		d.frame++
+		return false
+	} else {
+		d.frame = 0
+		return true
+	}
+}
+
+func (d *Digit) Morph8() bool {
+	if d.frame <= d.segLength {
+		d.drawLine(d.segLength - d.frame, d.segLength * 2 + 1, d.segLength - d.frame, d.segLength + 2, true)
+		if d.frame > 0 {
+			d.drawLine(d.segLength - d.frame + 1, d.segLength * 2 + 1, d.segLength - d.frame + 1, d.segLength + 2, false)
+		}
+		
+		d.drawLine(d.segLength - d.frame, 1, d.segLength - d.frame, d.segLength, true)
+		if d.frame > 0 {
+			d.drawLine(d.segLength - d.frame + 1, 1, d.segLength - d.frame + 1, d.segLength, false)
+		}
+		
+		if d.frame < d.segLength {
+			d.drawPixel(d.segLength - d.frame, 0, true)
+			d.drawPixel(d.segLength - d.frame, d.segLength + 1, true)
+		}
+		
+		d.frame++
+		return false
+	} else {
+		d.frame = 0
+		return true
+	}
+}
+
+func (d *Digit) Morph9() bool {
+	if d.frame <= d.segLength + 1 {
+		d.drawLine(d.frame - 1, 1, d.frame - 1, d.segLength, false)
+		d.drawLine(d.frame, 1, d.frame, d.segLength, true)
+		
+		d.frame++
+		return false
+	} else {
+		d.frame = 0
+		return true
+	}
+}
+
+func (d *Digit) Morph0() bool {
+	if d.frame <= d.segLength {
+		if d.value == 1 {
+			d.drawLine(d.segLength - d.frame, d.segLength * 2 + 1, d.segLength - d.frame, d.segLength + 2, true)
+			if d.frame > 0 {
+				d.drawLine(d.segLength - d.frame + 1, d.segLength * 2 + 1, d.segLength - d.frame + 1, d.segLength + 2, false)
+			}
+			
+			d.drawLine(d.segLength - d.frame, 1, d.segLength - d.frame, d.segLength, true)
+			if d.frame > 0 {
+				d.drawLine(d.segLength - d.frame + 1, 1, d.segLength - d.frame + 1, d.segLength, false)
+			}
+			
+			if d.frame < d.segLength {
+				d.drawPixel(d.segLength - d.frame, d.segLength * 2 + 2, true)
+				d.drawPixel(d.segLength - d.frame, 0, true)
+			}
+		}
+		
+		if d.value == 2 {
+			d.drawLine(d.segLength - d.frame, d.segLength * 2 + 1, d.segLength - d.frame, d.segLength + 2, true)
+			if d.frame > 0 {
+				d.drawLine(d.segLength - d.frame + 1, d.segLength * 2 + 1, d.segLength - d.frame + 1, d.segLength + 2, false)
+			}
+			
+			d.drawPixel(1 + d.frame, d.segLength + 1, false)
+			if d.frame < d.segLength {
+				d.drawPixel(d.segLength + 1, d.segLength + 1 - d.frame, true)
+			}
+		}
+		
+		if d.value == 5 {
+			if d.frame < d.segLength {
+				if d.frame > 0 {
+					d.drawLine(1 + d.frame, d.segLength * 2 + 1, 1 + d.frame, d.segLength + 2, false)
+				}
+				d.drawLine(2 + d.frame, d.segLength * 2 + 1, 2 + d.frame, d.segLength + 2, true)
+			}
+		}
+		
+		if d.value == 5 || d.value == 9 {
+			if d.frame < d.segLength {
+				d.drawPixel(d.segLength - d.frame, d.segLength + 1, false)
+				d.drawPixel(0, d.segLength - d.frame, true)
+			}
+		}
+		
+		d.frame++
+		return false
+	} else {
+		d.frame = 0
+		return true
+	}
+}
+
+func (d *Digit) Morph(value int) bool {
+	var done bool = false
+	switch value {
+		case 1:
+			done = d.Morph1()
+		case 2:
+			done = d.Morph2()
+		case 3:
+			done = d.Morph3()
+		case 4:
+			done = d.Morph4()
+		case 5:
+			done = d.Morph5()
+		case 6:
+			done = d.Morph6()
+		case 7:
+			done = d.Morph7()
+		case 8:
+			done = d.Morph8()
+		case 9:
+			done = d.Morph9()
+		case 0:
+			done = d.Morph0()
+	}
+	if done {
+		d.value = value
+	}
+	return done
+}
---
+++ MorphingClock.go
@@ -0,0 +1,136 @@
+package main
+
+import (
+	"flag"
+	"time"
+	"strconv"
+	"image"
+	"image/draw"
+	"github.com/Sunoo/go-rpi-rgb-led-matrix"
+)
+
+var (
+	rows                     = flag.Int("led-rows", 16, "number of rows supported")
+	cols                     = flag.Int("led-cols", 32, "number of columns supported")
+	parallel                 = flag.Int("led-parallel", 1, "number of daisy-chained panels")
+	chain                    = flag.Int("led-chain", 1, "number of displays daisy-chained")
+	brightness               = flag.Int("brightness", 100, "brightness (0-100)")
+	hardware_mapping         = flag.String("led-gpio-mapping", "regular", "Name of GPIO mapping used.")
+	show_refresh             = flag.Bool("led-show-refresh", false, "Show refresh rate.")
+	inverse_colors           = flag.Bool("led-inverse", false, "Switch if your matrix has inverse colors on.")
+	disable_hardware_pulsing = flag.Bool("led-no-hardware-pulse", false, "Don't use hardware pin-pulse generation.")
+)
+
+const (
+	/*xOrig = 1
+	yOrig = 2
+	segLength = 4*/
+	xOrig = -6
+	yOrig = 1
+	segLength = 6
+	leadingZero = false
+	timeFormat = "03:04"
+	animSpeed = 30
+)
+
+func main() {
+	config := &rgbmatrix.DefaultConfig
+	config.Rows = *rows
+	config.Cols = *cols
+	config.Parallel = *parallel
+	config.ChainLength = *chain
+	config.Brightness = *brightness
+	config.HardwareMapping = *hardware_mapping
+	config.ShowRefreshRate = *show_refresh
+	config.InverseColors = *inverse_colors
+	config.DisableHardwarePulsing = *disable_hardware_pulsing
+	
+	m, _ := rgbmatrix.NewRGBLedMatrix(config)
+
+	canvas := rgbmatrix.NewCanvas(m)
+	defer canvas.Close()
+	
+	d1 := NewDigit(segLength)
+	d2 := NewDigit(segLength)
+	co := NewColon(segLength)
+	d3 := NewDigit(segLength)
+	d4 := NewDigit(segLength)
+	
+	d1start := image.Point{xOrig, yOrig}
+	d2start := d1start.Add(image.Point{d1.img.Bounds().Max.X + 1, 0})
+	costart := d2start.Add(image.Point{d2.img.Bounds().Max.X + 1, 0})
+	d3start := costart.Add(image.Point{co.img.Bounds().Max.X + 1, 0})
+	d4start := d3start.Add(image.Point{d3.img.Bounds().Max.X + 1, 0})
+	
+	d1pos := image.Rectangle{d1start, d1start.Add(d1.img.Bounds().Max)}
+	d2pos := image.Rectangle{d2start, d2start.Add(d2.img.Bounds().Max)}
+	copos := image.Rectangle{costart, costart.Add(co.img.Bounds().Max)}
+	d3pos := image.Rectangle{d3start, d3start.Add(d3.img.Bounds().Max)}
+	d4pos := image.Rectangle{d4start, d4start.Add(d4.img.Bounds().Max)}
+	
+	initialTime := true
+	
+	for {
+		clock := time.Now().Format(timeFormat)
+		h1, _ := strconv.Atoi(clock[0:1])
+		h2, _ := strconv.Atoi(clock[1:2])
+		m1, _ := strconv.Atoi(clock[3:4])
+		m2, _ := strconv.Atoi(clock[4:5])
+
+		if initialTime {
+			draw.Draw(canvas, copos, co.img, image.ZP, draw.Src)
+			d4.Draw(m2)
+			draw.Draw(canvas, d4pos, d4.img, image.ZP, draw.Src)
+			d3.Draw(m1)
+			draw.Draw(canvas, d3pos, d3.img, image.ZP, draw.Src)
+			d2.Draw(h2)
+			draw.Draw(canvas, d2pos, d2.img, image.ZP, draw.Src)
+			if leadingZero || h1 != 0 {
+				d1.Draw(h1)
+				draw.Draw(canvas, d1pos, d1.img, image.ZP, draw.Src)
+			}
+		
+			canvas.RenderKeep()
+			initialTime = false
+		} else {
+			if m2 != d4.value {
+				done := false
+				for !done {
+					done = d4.Morph(m2)
+					draw.Draw(canvas, d4pos, d4.img, image.ZP, draw.Src)
+					canvas.RenderKeep()
+					time.Sleep(animSpeed * time.Millisecond)
+				}
+			}
+			if m1 != d3.value {
+				done := false
+				for !done {
+					done = d4.Morph(m1)
+					draw.Draw(canvas, d3pos, d3.img, image.ZP, draw.Src)
+					canvas.RenderKeep()
+					time.Sleep(animSpeed * time.Millisecond)
+				}
+			}
+			if h2 != d2.value {
+				done := false
+				for !done {
+					done = d2.Morph(h2)
+					draw.Draw(canvas, d2pos, d2.img, image.ZP, draw.Src)
+					canvas.RenderKeep()
+					time.Sleep(animSpeed * time.Millisecond)
+				}
+			}
+			if h1 != d1.value {
+				done := false
+				for !done {
+					done = d1.Morph(h1)
+					draw.Draw(canvas, d1pos, d1.img, image.ZP, draw.Src)
+					canvas.RenderKeep()
+					time.Sleep(animSpeed * time.Millisecond)
+				}
+			}
+		}
+		target := time.Now().Truncate(time.Minute).Add(time.Minute)
+		time.Sleep(target.Sub(time.Now()))
+	}
+}
